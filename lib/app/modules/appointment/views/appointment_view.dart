39255dd8b0c4ac4ed9ce9004e1e6bcf862012e44
akash <akashsreyas23@gmail.com>
2023-05-30 10:39:07 +0530

    all safe

--- lib/app/modules/appointment/views/appointment_view.dart
+++ lib/app/modules/appointment/views/appointment_view.dart
@@ -12,7 +12,7 @@ class AppointmentView extends GetView<AppointmentController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: ,
+      //new way,
       appBar: AppBar(
         backgroundColor: Styles.primaryBlueColor,
         title: Text('Appointment'.tr),
